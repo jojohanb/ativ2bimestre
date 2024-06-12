572043026f2f655d675ae2540598312c9e9ae5b5
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-06-12 11:56:06 -0300

    indo

--- ativ/index.js
+++ ativ/index.js
@@ -1,17 +1,37 @@
-var input= document.getElementById("tarefa")
+// var img= document.getElementsByClassName("linhona")
 var botao= document.getElementById("botao")
-var img= document.getElementsByClassName("linhona")
-
-
-botao.addEventListener("click", criarTarefas)
-
-function criarTarefas(){
-    for (var i = 1; i<=3 ; i++) {
-        let tarefas= document.createElement("p")
-
-        tarefas.textContent="alguma coisa sera add aqui"
-        
-        linhona.appendChild(tarefas)
+botao.addEventListener("click", function() {
+    var input= document.getElementById("tarefa").value
+    if (input.trim() !==""){
+        var p= document.createElement("p")
+        p.textContent=input.value
+        document.getElementsByClassName("linhona").appendChild(p)
+        document.getElementById("tarefa").value=""
+    } else {
+        alert("Digite uma tarefa")
     }
-}
+
+})
+
+
+
+
+
+//tentativa2
+// botao.addEventListener("click", criarTarefas)
+// function criarTarefas() {
+//     var pgf= document.createElement("p")
+//     var linhona = document.getElementById("linhona").appendChild(pgf)
+// }
+
+//tentativa1
+// function criarTarefas(){
+//     for (var i = 1; i<=3 ; i++) {
+//         let tarefas= document.createElement("p")
+
+//         tarefas.textContent="alguma coisa sera add aqui"
+        
+//         linhona.appendChild(tarefas)
+//     }
+// }
 
