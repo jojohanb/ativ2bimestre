f4cd309cd8ac55c2ae75cbb1a6d88d618ea05e7d
jojohanb <130059240+jojohanb@users.noreply.github.com>
2024-06-14 23:17:11 -0300

    indo

--- ativ/index.js
+++ ativ/index.js
@@ -1,17 +1,50 @@
-// var img= document.getElementsByClassName("linhona")
-var botao= document.getElementById("botao")
-botao.addEventListener("click", function() {
-    var input= document.getElementById("tarefa").value
-    if (input.trim() !==""){
-        var p= document.createElement("p")
-        p.textContent=input.value
-        document.getElementsByClassName("linhona").appendChild(p)
-        document.getElementById("tarefa").value=""
-    } else {
-        alert("Digite uma tarefa")
-    }
+var botao = document.getElementById("botao");
+var span1 = document.getElementById("span1");
+var linhona = document.getElementById("linhona");
+var img= document.getElementById("img")
+var msgs= document.getElementById("msgs")
 
-})
+
+botao.addEventListener("click", function() {
+
+    var input = document.getElementById("tarefa").value;
+
+    if (input.trim() !== ""){
+
+        var div= document.createElement("div")
+        div.className="checkbox-container"
+
+        var checkbox=document.createElement("input")
+        checkbox.type="checkbox"
+
+        var label= document.createElement("label")
+        label.textContent=input
+
+        var lixo= document.createElement("img")
+        lixo.src="/atv2/trash.png"
+        label.appendChild(lixo)
+
+        div.appendChild(checkbox)
+        div.appendChild(label)
+
+        linhona.appendChild(div)
+
+        // var p = document.createElement("p");
+        // p.textContent = input;
+        // linhona.appendChild(p);
+        document.getElementById("tarefa").value = "";
+
+        span1.textContent = parseInt(span1.textContent) + 1;
+        
+        img.style.display="none"
+        msgs.style.display="none"
+    }
+    
+    else {
+        alert("Digite uma tarefa");
+    }
+});
+
 
 
 
